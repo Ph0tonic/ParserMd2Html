2e3103ae7aba5730e4c17f8d8587573196fb6dc4
Bastien Wermeille <bastien.wermeille@gmail.com>
2019-01-13 10:21:48 +0100

    Add comment in test-all.py

--- test-all.py
+++ test-all.py
@@ -1,3 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Programe which use our compiler and compile all scss file located in the folder data, the final generated files are in the folder "generated".
+
+:returns: Nothing, but all css file are generated in the folder "generated" with the same name but the .css extension
+
+Correct syntax:
+python3 test-all.py filename
+
+Concrete example :
+python3 test-all.py "./data/_test-main.scss"
+
+Requirements:
+- Python3
+- Ply
+- Graphviz
+- pydot
+- yacc
+- AST.py
+- lex.py
+- recCompiler.py
+
+Authors:
+- Lucas Bulloni - https://github.com/bull0n
+- Bastien Wermeille - https://github.com/Ph0tonic
+
+Code source of the project:
+- https://github.com/Ph0tonic/SassCompiler
+"""
+
 import os
 import re
 from recCompiler import compile_file
